4e6a8bbd34ec979e4d2e30ee81f9ee37d90667c8
An3s079 <68040208+An3s079@users.noreply.github.com>
2021-01-21 14:06:48 -1000

    Add files via upload

---
+++ SwordtressModule.cs
@@ -0,0 +1,30 @@
+﻿using ItemAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Swordtress
+{
+    public class SwordtressModule : ETGModule
+    {
+        public static readonly string MOD_NAME = "Siege The Swordtress";
+        public static readonly string VERSION = "1.0.0";
+        public static readonly string TEXT_COLOR = "#00FFFF";
+
+        public override void Start()
+        {
+            ItemBuilder.Init();
+            Log($"{MOD_NAME} v{VERSION} started successfully.", TEXT_COLOR);
+        }
+
+        public static void Log(string text, string color="#FFFFFF")
+        {
+            ETGModConsole.Log($"<color={color}>{text}</color>");
+        }
+
+        public override void Exit() { }
+        public override void Init() { }
+    }
+}
